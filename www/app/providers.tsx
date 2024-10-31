6e08742d80987f819ce869adfb45013c826f8977
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2024-10-30 23:59:41 -0700

    posthog analytics

--- www/app/providers.tsx
+++ www/app/providers.tsx
@@ -1,10 +1,28 @@
 "use client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import type { ReactNode } from "react";
+
+import posthog from "posthog-js";
+import { PostHogProvider } from "posthog-js/react";
+
+if (typeof window !== "undefined") {
+	// biome-ignore lint/style/noNonNullAssertion: <explanation>
+	posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
+		// biome-ignore lint/style/noNonNullAssertion: <explanation>
+		person_profiles: "identified_only", // or 'always' to create profiles for anonymous users as well
+		api_host: "/ingest",
+		ui_host: "https://us.i.posthog.com",
+	});
+}
+export function CSPostHogProvider({ children }: { children: ReactNode }) {
+	return <PostHogProvider client={posthog}>{children}</PostHogProvider>;
+}
 const queryClient = new QueryClient();
 
 export default function Providers({ children }: { children: ReactNode }) {
 	return (
-		<QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+		<QueryClientProvider client={queryClient}>
+			<CSPostHogProvider>{children}</CSPostHogProvider>
+		</QueryClientProvider>
 	);
 }
--- www/next.config.mjs
+++ www/next.config.mjs
@@ -4,7 +4,18 @@ const withMDX = createMDX();
 
 /** @type {import('next').NextConfig} */
 const config = {
-	reactStrictMode: true,
+  reactStrictMode: true,
+  skipTrailingSlashRedirect: true,
+  rewrites: async () => [
+    {
+      source: "/ingest/static/:path*",
+      destination: "https://us-assets.i.posthog.com/static/:path*",
+    },
+    {
+      source: "/ingest/:path*",
+      destination: "https://us.i.posthog.com/:path*",
+    },
+  ],
 };
 
 export default withMDX(config);
